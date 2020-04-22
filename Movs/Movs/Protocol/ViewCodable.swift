c7ec6b9215b616b8ff9227d704111fabaad84e6f
Luis Gustavo <luis.gustavo3013@hotmail.com>
2020-04-22 13:32:59 -0300

    Created empty project

---
+++ Movs/Movs/Protocol/ViewCodable.swift
@@ -0,0 +1,9 @@
+//
+//  ViewCodable.swift
+//  Movs
+//
+//  Created by Luis Gustavo Avelino de Lima Jacinto on 22/04/20.
+//  Copyright © 2020 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
